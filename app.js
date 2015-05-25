017cbb41ab7c08db025b180fa776feed3f67785e
Heather Pike <heatherlaurenpike@gmail.com>
2015-05-24 23:47:06 -0700

    set up basic search requst functionality

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+var zenefitsLatLng, map;
+
 function initializeMap() {
 	//define the coordinates for the map
-	var zenefitsLatLng = new google.maps.LatLng(37.785, -122.396);
+	zenefitsLatLng = new google.maps.LatLng(37.785, -122.396);
 
 	//define map options, center and zoom level
 	var mapOptions = {
 	  center: zenefitsLatLng,
-	  zoom: 13
+	  zoom: 16
 	};
 
 	//find the map canvas element by id
 	var mapCanvas = $("#map-canvas")[0];
  
 	//create a map with the above specified options
-	var map = new google.maps.Map(mapCanvas, mapOptions);
+	map = new google.maps.Map(mapCanvas, mapOptions);
 
 	//define a marker for the map center: Zenefits HQ ;)
 	var marker = new google.maps.Marker({
@@ -21,10 +23,50 @@ function initializeMap() {
         icon: "zenefits_bird.png"
     });
 
-    // Add a main marker to the map 
+    // Add the main marker to the map 
     marker.setMap(map);
 }
 
 $(document).ready(function() {
 	initializeMap();
-})
+
+});
+
+var infoWindow;
+
+function searchPlaces() {
+
+	var request = {
+		types: ['food', 'restaurant', 'meal_takeaway', 'meal_delivery'],
+		keywords: $("input").val(),
+		location: zenefitsLatLng,
+		radius: 500,
+		openNow: true
+	}
+
+	infoWindow = new google.maps.InfoWindow();
+	var service = new google.maps.places.PlacesService(map);
+	  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(place.name);
+    infoWindow.open(map, this);
+  });
+}
+
