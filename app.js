b24e68d52cc766b9c8add4a6df9a00956dcd48fb
Heather Pike <heatherlaurenpike@gmail.com>
2015-05-24 22:25:18 -0700

    customized map

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-function initialize() {
+function initializeMap() {
+	//define the coordinates for the map
+	var zenefitsLatLng = new google.maps.LatLng(37.785, -122.396);
+
+	//define map options, center and zoom level
 	var mapOptions = {
-	  center: { lat: 37.7833, lng: -122.4167},
+	  center: zenefitsLatLng,
 	  zoom: 13
 	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'),
-	    mapOptions);
+
+	//find the map canvas element by id
+	var mapCanvas = $("#map-canvas")[0];
+ 
+	//create a map with the above specified options
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+
+	//define a marker for the map center: Zenefits HQ ;)
+	var marker = new google.maps.Marker({
+        position: zenefitsLatLng,
+        title: "Zenefits HQ",
+        icon: "zenefits_bird.png"
+    });
+
+    // Add a main marker to the map 
+    marker.setMap(map);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function() {
-
+	initializeMap();
 })
