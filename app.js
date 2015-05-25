b553b6c0f814105b731233ad464a75896f4f3d67
Heather Pike <heatherlaurenpike@gmail.com>
2015-05-24 21:01:51 -0700

    created map instance

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var app = angular.module("GooglePlaces", []);
+function initialize() {
+	var mapOptions = {
+	  center: { lat: 37.7833, lng: -122.4167},
+	  zoom: 13
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+	    mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 
-app.controller("MainController", function ($scope) {
-});
+$(document).ready(function() {
+
+})
